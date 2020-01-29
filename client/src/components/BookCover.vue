73ee2def67501f82bb24daa5b10e782670047111
Luis Güette <guetteluis@gmail.com>
2020-01-28 22:18:46 -0400

    add waitlist

--- client/src/components/BookCover.vue
+++ client/src/components/BookCover.vue
@@ -1,7 +1,20 @@
 <template>
-    <div class="w-32 flex-shrink-0">
-        <div class="relative py-24">
-            <img class="absolute inset-0 h-full rounded-lg object-cover border" src="https://covers.openlibrary.org/b/id/9256153-L.jpg" alt="">
+    <div>
+        <div class="flex-shrink-0" :class="[sm ? 'w-20' : 'w-32']">
+            <div class="relative" :class="[sm ? 'py-16' : 'py-24']">
+                <img class="absolute inset-0 h-full rounded-lg object-cover border" src="https://covers.openlibrary.org/b/id/9256153-L.jpg" alt="">
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        sm: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -14,8 +14,8 @@
                 </label>
 
                 <div class="px-4 overflow-x-auto">
-                    <div class="flex mt-4 -mx-4">
-                        <BookCover v-for="i in 10" :key="i"/>
+                    <div class="flex mt-4 -ml-12">
+                        <BookCover v-for="i in 10" :key="i" class="pl-8"/>
                     </div>
                 </div>
             </card>
@@ -26,8 +26,19 @@
                         <h2 class="text-gray-600 text-2xl">Your waitlist</h2>
                         <router-link to="/books/create" class="text-indigo-500 hover:underline">New Book</router-link>
                     </div>
-                    <div class="p-4">
-                        <p>content</p>
+                    <div v-for="i in 10" :key="i">
+                        <div class="flex justify-start p-4">
+                            <BookCover sm/>
+                            <div class="pl-4">
+                                <h4 class="block text-xl text-indigo-500">Harry Potter sd sahsas skjhas </h4>
+                                <span class="block text-gray-600">Phoenyx order</span>
+                                <span class="text-gray-600">by</span> <span class="text-indigo-500">J. K. Rowling</span>
+                                <button class="block mt-2 px-4 py-1 bg-red-100 rounded">
+                                    <span class="text-red-500 hover:text-red-700">Delete</span>
+                                </button>
+                            </div>
+                        </div>
+                        <hr class="border" />
                     </div>
                 </card>
             </div>
